b51cf04cd290149aa1d3d5f90e1ec7da107e86dc
borgolov <79781059+borgolov@users.noreply.github.com>
2021-07-31 15:24:10 +0800

    update flask tp 2.0.1, remove flask-script, now manipulation with the database occurs through the commands: flask db init, flask db migrate, flask db upgrade

--- app/frontend/views.py
+++ app/frontend/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, current_app, send_from_directory, redirect
+from flask import render_template, request, current_app, send_from_directory, redirect, jsonify
 
 from app.frontend import frontend
 
@@ -6,3 +6,7 @@ from app.frontend import frontend
 @frontend.route('/favicon.ico')
 def favicon():
     return redirect('/static/favicon.png')
+
+@frontend.route('/')
+def main():
+    return jsonify({"code": 200})
--- run.py
+++ run.py
@@ -1,22 +1,13 @@
-from flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager
+from flask_migrate import Migrate
 
 from app import create_app
 from app.extensions import db, socket_io
 
 app = create_app()
 
-manager = Manager(app)
-
 migrate = Migrate(app, db)
 
-manager.add_command('db', MigrateCommand)
-
-
-@manager.command
-def runserver():
-    socket_io.run(app)
 
 
 if __name__ == '__main__':
-    manager.run()
+    socket_io.run(app)
