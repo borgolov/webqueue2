8325f386487fcfd9962818f1634567ea29470b49
borgolov <79781059+borgolov@users.noreply.github.com>
2024-07-20 23:10:42 +0800

    20.07.2024

--- app/static/screen_new/js/screenapp.js
+++ app/static/screen_new/js/screenapp.js
@@ -5,7 +5,7 @@ const app = Vue.createApp({
             axios: null,
             room_id: "",
             location: "location",
-            organization: "organiztion",
+            organization: "organization",
             device: {
                 id: null,
                 name: "device_name"
@@ -19,10 +19,10 @@ const app = Vue.createApp({
                 operator: '',
                 ticket: ''
             },
-            notifiations: [],
+            notifications: [],
             stat: {},
             modal: false,
-            settings: !1,
+            settings: false,
             voices: [],
             voic: null,
             utterance: null,
@@ -31,7 +31,7 @@ const app = Vue.createApp({
             voice_settings: {
                 volume: 1,
                 rate: 1,
-                pitch:1
+                pitch: 1
             },
             audio_settings: {
                 volume: 1
@@ -40,61 +40,65 @@ const app = Vue.createApp({
     },
     created() {
         this.socket = io('/queue');
-        this.axios = axios
-        this.get_voices()
+        this.axios = axios;
+        this.get_voices();
 
-        setTimeout(this.init(), 1000)
+        this.$nextTick(() => {
+            this.init();
+            this.set_voic();
+        });
 
         this.socket.on('call_client', (data) => {
-            this.notifiations.push(data)
-            this.show_modal()
+            this.notifications.push(data);
+            this.show_modal();
         });
         this.socket.on('state', (data) => {
-            this.stat = data
+            this.stat = data;
         });
-        setTimeout(this.set_voic(), 1000)
     },
     methods: {
         init() {
             this.axios.get(this.urls[0]).then(resp => {
-                this.location = resp.data.location
-                this.organization = resp.data.organization
-                this.device = resp.data.device
+                this.location = resp.data.location;
+                this.organization = resp.data.organization;
+                this.device = resp.data.device;
             }).catch(error => {
-                console.log(error)
-            })
+                console.error('Error fetching device settings:', error);
+            });
         },
         show_modal() {
-            var notific = this.notifiations[0]
-            this.last_notific.operator = notific?.operator?.name
-            this.last_notific.ticket = notific?.ticket.prefix + notific?.ticket.num
-            if (this.notifiations.length > 0 && this.modal == false) {
-                this.modal = true
+            if (this.notifications.length === 0) return;
+
+            const notific = this.notifications[0];
+            this.last_notific.operator = notific?.operator?.name;
+            this.last_notific.ticket = notific?.ticket.prefix + notific?.ticket.num;
+
+            if (!this.modal) {
+                this.modal = true;
                 this.suound_signal(() => {
-                    if (this.voic != null){
-                        this.voice(notific, this.last_notific.ticket)
-                    }
-                    else {
+                    if (this.voic !== null) {
+                        this.voice(notific, this.last_notific.ticket);
+                    } else {
                         setTimeout(() => {
-                            this.modal = false
-                            this.notifiations.shift()
+                            this.modal = false;
+                            this.notifications.shift();
                             setTimeout(() => {
-                                this.show_modal()
-                            }, 500)
-                        }, 3000)
+                                this.show_modal();
+                            }, 500);
+                        }, 3000);
                     }
-                })          
+                });
             }
         },
         voice(notific, string) {
-            this.utterance = new SpeechSynthesisUtterance(notific?.operator?.duber.replace('@', string))
+            this.utterance = new SpeechSynthesisUtterance(notific?.operator?.duber.replace('@', string));
             this.utterance.addEventListener('end', () => {
-                this.notifiations.shift()
+                this.notifications.shift();
                 this.modal = false;
                 setTimeout(() => {
                     this.show_modal();
-                }, 500)
-            })
+                }, 500);
+            });
             this.utterance.voice = this.voic;
             this.utterance.volume = this.voice_settings.volume;
             this.utterance.pitch = this.voice_settings.pitch;
@@ -102,41 +106,36 @@ const app = Vue.createApp({
             speechSynthesis.speak(this.utterance);
         },
         cli() {
-            console.log("click")
+            console.log("click");
         },
         get_voices() {
-            this.voices = window.speechSynthesis.getVoices()
+            this.voices = window.speechSynthesis.getVoices();
+            window.speechSynthesis.onvoiceschanged = () => {
+                this.voices = window.speechSynthesis.getVoices();
+            };
         },
         show_voices_select() {
-            this.get_voices()
-            if (!this.modal_settings) {
-                this.modal_settings = true;
-                return
-            }
-            this.modal_settings = false;
+            this.get_voices();
+            this.modal_settings = !this.modal_settings;
         },
         set_voic() {
             for (let i = 0; i < this.voices.length; i++) {
                 if (this.voices[i].name === 'Google русский') {
-                    this.voic = voices[i];
+                    this.voic = this.voices[i];
                     break;
                 }
             }
         },
         suound_signal(callback) {
             const audio = new Audio(this.urls[2]);
-            // Добавляем обработчик события ended
-            audio.addEventListener('ended', function () {
-                // Этот код будет выполнен после завершения воспроизведения аудио
+            audio.addEventListener('ended', () => {
                 console.log('Звук закончился');
                 callback();
             });
-            // Воспроизводим звук
             audio.volume = this.audio_settings.volume;
-            if (this.is_screen_sound){
+            if (this.is_screen_sound) {
                 audio.play();
-            }
-            else {
+            } else {
                 callback();
             }
         }
